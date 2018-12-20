506fac1d426b578bf86f471bbc4729325a114f61
Devendranath <dnreddy890@gmail.com>
2018-12-20 09:32:54 +0530

    Initial Commit

---
+++ CompleteSwiftPractical/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CompleteSwiftPractical
+//
+//  Created by apple on 20/12/18.
+//  Copyright © 2018 iOSProofs. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
